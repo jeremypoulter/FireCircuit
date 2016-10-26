a82aa1ee948e1979cdd33a7b3f1bfd92a6839972
Jeremy Poulter <jeremy@bigjungle.net>
2016-10-26 09:01:34 +0100

    Changed so the animation is only displayed for 5 hours before going back to sleep

--- FireCircuit.ino
+++ FireCircuit.ino
@@ -10,22 +10,28 @@ void setup() {
   pinMode(ON_LED, OUTPUT);
 }
 
-#define CAP_THRESHOLD    250  // Threshold for a capacitive touch (higher = less sensitive).
+#define CAP_THRESHOLD    150  // Threshold for a capacitive touch (higher = less sensitive).
 #define CAP_SAMPLES      20   // Number of samples to take for a capacitive touch read.
 
 uint8_t prev[] = {128, 128};              // Start brightness in middle
 float lightLevel = 100;
 
 #define LIGHT_ON_INFLUANCE 0.05
-#define LIGHT_OFF_THRESHOLD 250
-#define LIGHT_ON_THRESHOLD  300
+#define LIGHT_OFF_THRESHOLD 300
+#define LIGHT_ON_THRESHOLD  200
 #define LIGHT_UPDATE_TIME (1000/10)
 
 bool displayOn = false;
 long lightReadTimeout = 0;
 
+long displayOffTimeOut = 0;
+#define DISPLAY_TIME_ON (5 * 60 * 60 * 1000)
+//#define DISPLAY_TIME_ON (10 * 1000)
+
 void loop() 
 {
+  bool sleep = false;
+  
   if(CircuitPlayground.slideSwitch())
   {
     digitalWrite(ON_LED, HIGH);
@@ -34,7 +40,7 @@ void loop()
     {
       int value = CircuitPlayground.lightSensor();
       lightLevel = displayOn ?
-                    (float)lightLevel * (1-LIGHT_ON_INFLUANCE) + (float)value * LIGHT_ON_INFLUANCE :(float)value;
+                    (float)lightLevel * (1-LIGHT_ON_INFLUANCE) + (float)value * LIGHT_ON_INFLUANCE : (float)value;
       
       //Serial.print("Light Sensor: ");
       Serial.print(value);
@@ -46,40 +52,54 @@ void loop()
     int lightThreshold = displayOn ? LIGHT_OFF_THRESHOLD : LIGHT_ON_THRESHOLD;
     if(lightLevel <= lightThreshold)
     {
-      displayOn = true;
-      int cap = CircuitPlayground.readCap(0, CAP_SAMPLES);
-      Serial.println(cap);
-      if(cap >= CAP_THRESHOLD) 
+      if(false == displayOn)
       {
-        uint32_t offset = millis() / 5;
-        // Loop through each pixel and set it to an incremental color wheel value.
-        for(int i=0; i<10; ++i) {
-          CircuitPlayground.strip.setPixelColor(i, CircuitPlayground.colorWheel(((i * 256 / 10) + offset) & 255));
-        }
-        // Show all the pixels.
-        CircuitPlayground.strip.show();
+        // Turning the animations on start the timer
+        displayOffTimeOut = millis() + DISPLAY_TIME_ON;
+        displayOn = true;
       }
-      else
+
+      if(millis() < displayOffTimeOut)
       {
-        for (int i = 0; i < 2; i++)
+        int cap = CircuitPlayground.readCap(0, CAP_SAMPLES);
+        Serial.println(cap);
+        if(cap >= CAP_THRESHOLD) 
         {
-          uint8_t lvl = random(64, 192);            // End brightness at 128±64
-          split(prev[i], lvl, 32, i*5, 5 + (i*5));  // Start subdividing, ±32 at midpoint
-          prev[i] = lvl;                            // Assign end brightness to next start
+          uint32_t offset = millis() / 5;
+          // Loop through each pixel and set it to an incremental color wheel value.
+          for(int i=0; i<10; ++i) {
+            CircuitPlayground.strip.setPixelColor(i, CircuitPlayground.colorWheel(((i * 256 / 10) + offset) & 255));
+          }
+          // Show all the pixels.
+          CircuitPlayground.strip.show();
         }
+        else
+        {
+          for (int i = 0; i < 2; i++)
+          {
+            uint8_t lvl = random(64, 192);            // End brightness at 128±64
+            split(prev[i], lvl, 32, i*5, 5 + (i*5));  // Start subdividing, ±32 at midpoint
+            prev[i] = lvl;                            // Assign end brightness to next start
+          }
+        }
+      } else {
+        sleep = true;
       }
     } else {
       displayOn = false;
+      sleep = true;
     }
+
+    digitalWrite(ON_LED, LOW);
   }
   else
   {
-    digitalWrite(ON_LED, LOW);
     displayOn = false;
+    sleep = true;
   }
 
   // If the display is off, turn off the LEDs and sleep for a second
-  if(false == displayOn)
+  if(sleep)
   {
     for(uint8_t i=0; i<10; i++) CircuitPlayground.strip.setPixelColor(i, 0);
     CircuitPlayground.strip.show();
